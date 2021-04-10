a42274017c39d532cee2985243008845575b665d
Clement Denis <le.mikmac@gmail.com>
2021-04-10 15:34:51 +0100

    app: add react akwaba

--- script/start.sh
+++ script/start.sh
@@ -1,6 +1,7 @@
 esbuild app.jsx \
   --bundle \
-  --define:process.env.NODE_ENV='"production"' \
+  --define:process.env.NODE_ENV='"developement"' \
   --format=esm \
   --inject:lib/react-shim.js \
+  --servedir=public \
   --outdir=public/js
