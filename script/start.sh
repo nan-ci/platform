ec79b7e0bc99a5db2e9b80122b85eb9fd97fc1aa
Clement Denis <le.mikmac@gmail.com>
2021-04-10 15:52:43 +0100

    app: add sourcemap for dev and optimise bundle for prod

--- script/start.sh
+++ script/start.sh
@@ -3,5 +3,6 @@ esbuild app.jsx \
   --define:process.env.NODE_ENV='"developement"' \
   --format=esm \
   --inject:lib/react-shim.js \
+  --sourcemap=inline \
   --servedir=public \
   --outdir=public/js
