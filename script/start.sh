35830d67103780fea6a41411a4227a2686cf6739
Clement Denis <le.mikmac@gmail.com>
2021-04-11 02:13:04 +0100

    app: use preact instead of react

--- script/start.sh
+++ script/start.sh
@@ -2,7 +2,9 @@ node_modules/.bin/esbuild app.jsx \
   --bundle \
   --define:process.env.NODE_ENV='"developement"' \
   --format=esm \
-  --inject:lib/react-shim.js \
+  --jsx-factory=h \
+  --jsx-fragment=Fragment \
+  --inject:lib/preact-shim-dev.js \
   --sourcemap=inline \
   --servedir=public \
   --outdir=public/js
