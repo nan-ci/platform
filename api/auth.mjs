c4b954b48adcfbb0d664cac3ded587cc9a6ce08e
Clement Denis <le.mikmac@gmail.com>
2021-04-11 02:18:45 +0100

    api: add discord and github auth

---
+++ api/auth.mjs
@@ -0,0 +1,154 @@
+import { BAD_REQUEST, UNAUTHORIZED, TYPE_JSON, INTERNAL, rand } from './defs.mjs'
+import { GET, withUser } from './router.mjs'
+import * as db from './db.mjs'
+
+const gql = async (query) => {
+  const text = await query.text()
+  try {
+    const { data, errors } = JSON.parse(text)
+    return { data, error: errors && errors[0].message }
+  } catch {
+    return { error: text }
+  }
+}
+
+const GUILD = '821033851153285170'
+const DISCORD = 'https://discordapp.com/api'
+GET.auth.discord = async ({ url }) => {
+  const code = url.searchParams.get('code')
+  const state = url.searchParams.get('state')
+  if (!code || !state) return new Response('Missing Params', BAD_REQUEST)
+  const session = await db.get(`discord:${state}`)
+  if (!session?.user) return new Response('Bad State', BAD_REQUEST)
+  const authResponse = await fetch(`${DISCORD}/oauth2/token`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: new URLSearchParams({
+      scope: 'identify gdm.join guilds.join',
+      client_id: '826974634069983282',
+      client_secret: DISCORD_SECRET,
+      grant_type: 'authorization_code',
+      // redirect_uri: redirectUri, do i need dis ?
+      code,
+    }),
+  })
+
+  const auth = await authResponse.json()
+  if (auth.error) return new Response(auth.error_description, BAD_REQUEST)
+  const userResponse = await fetch(`${DISCORD}/users/@me`, {
+    headers: { Authorization: `Bearer ${auth.access_token}` },
+  })
+  const { id: discordId } = await userResponse.json()
+  const user = { ...session.user, discordId }
+  const pendingUpdate = db.set(session.name, user)
+  // join discord server
+  const join = await fetch(`${DISCORD}/guilds/${GUILD}/members/${discordId}`, {
+    method: 'PUT',
+    headers: { Authorization: `Bot ${BOT_TOKEN}`, ...TYPE_JSON },
+    body: JSON.stringify({
+      nick: user.name ? `${user.login} (${user.name})` : user.login,
+      access_token: auth.access_token,
+      roles: ['826978112054100008'],
+    }),
+  })
+
+  if (!join.ok) {
+    console.error('Unable to join discord:', join.statusText)
+  }
+
+  await pendingUpdate
+
+  const Location = `/?${new URLSearchParams(user)}`
+  return new Response(null, { headers: { Location }, status: 301 })
+}
+
+GET.auth.github = async ({ url: { searchParams } }) => {
+  const state = searchParams.get('state')
+  const code = searchParams.get('code')
+  if (!state || !code) return new Response(null, BAD_REQUEST)
+  const access = await db.get(`github:${state}`)
+  if (!access) return new Response(null, UNAUTHORIZED)
+
+  // get github token
+  const res = await fetch('https://github.com/login/oauth/access_token', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+    body: String(
+      new URLSearchParams({
+        client_id: '07957ad810a70e99d67c',
+        client_secret: GITHUB_SECRET,
+        state, // is the state needed here ?
+        code,
+      }),
+    ),
+  })
+
+  const authBody = new URLSearchParams(await res.text())
+  const token = authBody.get('access_token')
+
+  if (authBody.get('error')) {
+    return new Response(authBody.get('error_description'), BAD_REQUEST)
+  }
+
+  // get github user data from the token
+  const query = await fetch('https://api.github.com/graphql', {
+    method: 'POST',
+    headers: {
+      Authorization: `bearer ${token}`,
+      'User-Agent': 'nan-academy/nan-platform',
+      ...TYPE_JSON,
+    },
+    body: JSON.stringify({ query: '{ viewer { login id name }}' }),
+  })
+
+  const { data, error } = await gql(query)
+  if (error) return new Response(error, INTERNAL)
+
+  // create the user session
+  const { id, login, name } = data.viewer
+  const sid = Number(atob(id).slice(7)).toString(36)
+  const key = await db.find(`user:${sid}:`)
+  const session =
+    key?.name || `user:${sid}:${login}:${Date.now().toString(36)}:${rand()}`
+  const user = { sid, login, name }
+  await db.set(session, user)
+  return new Response(null, {
+    status: 301,
+    headers: {
+      Location: `/?${new URLSearchParams(user)}`,
+      'Set-Cookie': [
+        `nan-session=${session}`,
+        'path=/',
+        'domain=nan.oct.ovh',
+        'HttpOnly',
+        'SameSite=Strict',
+        'Secure',
+      ].join('; '),
+    },
+  })
+}
+
+const oauth2Url = (url, args) => `https://${url}?${new URLSearchParams(args)}`
+GET.link.discord = withUser(async ({ user, session }) => {
+  const state = `${rand()}-${rand()}`
+  const metadata = { user, name: session }
+  await db.put(`discord:${state}`, '', { expirationTtl: 3600, metadata })
+  const Location = oauth2Url('discordapp.com/api/oauth2/authorize', {
+    client_id: '826974634069983282',
+    response_type: 'code',
+    scope: 'identify gdm.join guilds.join',
+    state,
+  })
+  return new Response(null, { headers: { Location }, status: 301 })
+})
+
+GET.link.github = async () => {
+  const state = `${rand()}-${rand()}`
+  await db.put(`github:${state}`, '', { expirationTtl: 3600, metadata: {} })
+  const Location = oauth2Url('github.com/login/oauth/authorize', {
+    client_id: `07957ad810a70e99d67c`,
+    scope: 'user',
+    state,
+  })
+  return new Response(null, { headers: { Location }, status: 301 })
+}
