8e38c7d9fca91de14482446ac60f7e671adf8c19
Clement Denis <le.mikmac@gmail.com>
2021-04-15 12:35:05 +0100

    config: restart tests after file changes

--- script/start.sh
+++ script/start.sh
@@ -4,5 +4,7 @@ export NODE_ENV="${NODE_ENV:-developement}"
 
 [ ! -f node_modules/.bin/esbuild ] && npm install
 
-fd --extension js \
- | entr -cdr node dev/server.js
+while true; do
+  fd --extension js \
+   | entr -cdr node dev/server.js
+done
