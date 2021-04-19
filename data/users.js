3197585b0eb119418915981dd54731b3e72728f9
emmadal <emmanueldalougou@gmail.com>
2021-04-19 08:47:57 +0000

    student list

---
+++ data/users.js
@@ -0,0 +1,30 @@
+export const users = [
+  {
+    name: 'user:4ytg:kigiri:knerxnaq:6zv0880wpd',
+    metadata: {
+      sid: '4ytg',
+      login: 'kigiri',
+      name: 'Clément',
+    },
+  },
+  {
+    name: 'user:muqzc:emmadal:knnih4n8:e92cmlarux',
+    metadata: {
+      sid: 'muqzc',
+      login: 'emmadal',
+      name: 'emmadal',
+      discordId: '815974440831549521',
+      email: 'emmadalcoder@gmail.com',
+      avatar: null,
+      speciality: 'javascript',
+    },
+  },
+  {
+    name: 'user:s95ha:ramseycoder:knerszzm:q90ra7dcu6',
+    metadata: {
+      sid: 's95ha',
+      login: 'ramseycoder',
+      name: 'Rameaux koffi',
+    },
+  },
+]
