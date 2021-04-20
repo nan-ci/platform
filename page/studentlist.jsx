1fb26b466105709dfdce3fe51f748e6f1bc67581
emmadal <emmanueldalougou@gmail.com>
2021-04-20 16:59:45 +0000

    change method to get unique column

--- page/studentlist.jsx
+++ page/studentlist.jsx
@@ -5,28 +5,27 @@ import { users } from '../data/users.js'
 
 export const StudentList = () => {
   const [data, setData] = useState([])
-
-  const columns = [
-    'sid',
-    'login',
-    'name',
-    'discordId',
-    'email',
-    'avatar',
-    'speciality',
-  ]
+  const [columns, setColumns] = useState([])
 
   const extractMetaData = (item) => item.metadata
 
+  const getColumn = () => {
+    const arr = []
+    for (const user of users) {
+      for (const element of Object.keys(user.metadata)) {
+        arr.push(element)
+      }
+    }
+    setColumns([...new Set(arr)])
+  }
+
   useEffect(() => setData(users), [])
+  useEffect(() => getColumn(), [])
 
   return data.length > 0 ? (
     <Main>
       <span>Student list</span>
-      <Table
-        data={data.map(extractMetaData)}
-        columns={columns}
-      />
+      <Table data={data.map(extractMetaData)} columns={columns} />
     </Main>
   ) : (
     <span> No student found!</span>
