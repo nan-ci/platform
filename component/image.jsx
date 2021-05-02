1a9e1033aebe025fc5ad67efc5985ef787ef6c42
emmadal <emmanueldalougou@gmail.com>
2021-05-02 11:17:40 +0000

    add alt props on image component

--- component/image.jsx
+++ component/image.jsx
@@ -2,7 +2,7 @@ import { h } from 'preact'
 
 export const Img = ({ size, ...props }) => {
   const img = new Image(size, size)
-  img.alt = props.description
   const src = (img.src = props.source.length > 0 ? props.source : '')
-  return h('img', { src }, null)
+  const alt = props.description || undefined
+  return h('img', { src, alt }, null)
 }
