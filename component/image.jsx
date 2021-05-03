3ddf76227be546be9e2d1f254586deb38161f08e
emmadal <emmanueldalougou@gmail.com>
2021-05-03 18:41:53 +0000

    refactoring login page

--- component/image.jsx
+++ component/image.jsx
@@ -1,4 +1,8 @@
+import { h } from 'preact'
 
-export const Img = ({source,alt,...rest}) => {
-   return <img src={source.length > 0 ? '/assets/img/'+source : ''} alt={alt} {...rest} />
+export const Img = ({ size, ...props }) => {
+  const img = new Image(size, size)
+  const src = (img.src = `/assets/img/${props.source}` || '')
+  const alt = props.description || undefined
+  return h('img', { src, alt, width: size, height: size }, null)
 }
--- page/auth.jsx
+++ page/auth.jsx
@@ -1,15 +1,28 @@
-import {useEffect} from 'preact/hooks';
-import {Link} from '../component/elements.jsx'
-import {Img} from '../component/image.jsx';
-import {css,link} from '../lib/dom.js';
+import { useEffect } from 'preact/hooks'
+import { Link } from '../component/elements.jsx'
+import { Img } from '../component/image.jsx'
+import { css, link } from '../lib/dom.js'
 import { API } from '../lib/env.js'
-import {Github,Discord} from '../component/icons.jsx';
+import { Github, Discord } from '../component/icons.jsx'
 
-link("https://fonts.gstatic.com","preconnect");
-link("https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap","stylesheet");
-link("https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@300;400;500;600;700&display=swap","stylesheet");
+// link('https://fonts.gstatic.com', 'preconnect')
+// link(
+//   'https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap',
+//   'stylesheet',
+// )
+// link(
+//   'https://fonts.googleapis.com/css2?family=Open+Sans&family=Poppins:wght@300;400;500;600;700&display=swap',
+//   'stylesheet',
+// )
 
 css(`
+.login-page {
+  position: relative;
+  min-height: 100vh;
+  display: flex; }
+  @media only screen and (max-width: 62em) {
+    .login-page {
+      display: block; } }
 .lg-illustration {
   flex: 1;
   background: linear-gradient(#15151d61, #15151d), url(/assets/img/christina.jpg);
@@ -29,8 +42,6 @@ css(`
       height: 25rem;
       background-position-y: -62px; } }
   .lg-illustration img {
-    width: 100%;
-    height: 100%;
     object-fit: cover;
     opacity: .1; }
 
@@ -54,97 +65,6 @@ css(`
   @media only screen and (max-width: 36em) {
     .lg-title {
       font-size: 3.5rem; } }
-
-.form-login {
-  text-align: left;
-  margin: 0 auto;
-  width: 37rem; }
-  @media only screen and (max-width: 36em) {
-    .form-login {
-      width: 100%; } }
-  .form-login .input-group {
-    margin-bottom: 2rem; }
-    .form-login .input-group input {
-      font-size: 1.7rem;
-      padding: 1.4rem 2rem;
-      border-radius: 5px;
-      background-color: transparent;
-      border: 1px solid #2d2e3b;
-      color: #f4f2f2;
-      width: 100%; }
-      .form-login .input-group input::placeholder {
-        color: #f2f3f465; }
-      .form-login .input-group input:focus {
-        border: 1px solid #545dd3;
-        outline: none; }
-  .form-login button {
-    background-color: #545dd3;
-    color: #f4f2f2;
-    font-size: 1.7rem;
-    padding: 1.3rem 1.8rem;
-    border: none;
-    width: 100%;
-    border-radius: 3px;
-    cursor: pointer;
-    margin-top: 2rem;
-    margin-bottom: 2rem;
-    transition: all .2s; }
-    .form-login button:hover {
-      background-color: #484fb1; }
-  .form-login a {
-    font-size: 1.4rem;
-    color: #b0b0bd;
-    font-weight: 300; }
-    .form-login a:hover {
-      text-decoration: underline; }
-
-.form .input-group {
-  margin-bottom: 2rem; }
-  .form .input-group label {
-    text-transform: uppercase;
-    font-weight: 600;
-    color: #b0b0bd9d;
-    font-size: 1.2rem;
-    margin-bottom: 1rem;
-    display: block; }
-  .form .input-group input {
-    font-size: 1.7rem;
-    padding: 1.4rem 2rem;
-    border-radius: 5px;
-    background-color: transparent;
-    border: 1px solid #2d2e3b;
-    color: #f4f2f2;
-    width: 100%; }
-    .form .input-group input.--limited {
-      max-width: 50rem; }
-    .form .input-group input.--bg-d {
-      background-color: #15151d; }
-    .form .input-group input::placeholder {
-      color: #f2f3f465; }
-    .form .input-group input:focus {
-      border: 1px solid #545dd3;
-      outline: none; }
-
-.lg-separator {
-  margin-top: 5rem;
-  line-height: 1px;
-  margin-bottom: 5rem; }
-  @media only screen and (max-width: 62em) {
-    .lg-separator {
-      width: 50rem;
-      margin: 5rem auto; } }
-  @media only screen and (max-width: 36em) {
-    .lg-separator {
-      width: 100%; } }
-
-.lg-divider {
-  border: 1px solid #23242e; }
-
-.lg-separator span {
-  color: #b0b0bd;
-  font-size: 1.4rem;
-  background-color: #14141c;
-  padding: 1rem 2rem; }
 
 .social-connexion {
   display: flex;
@@ -186,17 +106,16 @@ css(`
 }
 `)
 
-
 export const Auth = () => {
- useEffect(() => {
-  document.body.setAttribute('class',"simple-theme");
-  document.body.firstChild.setAttribute('class','login-page');
- }, [])
+  useEffect(() => {
+    document.body.setAttribute('class', 'simple-theme')
+    document.body.firstChild.setAttribute('class', 'login-page')
+  }, [])
 
   return (
     <>
       <div className="lg-illustration">
-        <Img source="logo-nan.png" alt="logo" />
+        <Img source="logo-nan.png" description="logo" size={500} />
       </div>
       <div className="login-box">
         <div>
@@ -221,7 +140,7 @@ export const Auth = () => {
             </li>
           </ul>
         </div>
-      </div>{' '}
+      </div>
     </>
   )
 }
