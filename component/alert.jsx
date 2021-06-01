ea177e341bbedac94eac6cfc0f90fcaf6aea718e
emmadal <emmanueldalougou@gmail.com>
2021-06-01 23:36:55 +0000

    add alert component

---
+++ component/alert.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'preact/hooks'
+import { css } from '../lib/dom.js'
+import { Div, P } from './elements.jsx'
+
+css(`
+.alert-container{
+  position: relative;
+  padding: .75rem 1.25rem;
+  margin: 1rem 0px;
+  border: 1px solid transparent;
+  border-radius: .25rem;
+}
+.dismiss-btn{
+  position: absolute;
+  top: 0;
+  right: 0;
+  padding: .75rem 1.25rem;
+  color: inherit;
+  outline: none;
+  cursor: pointer;
+}
+`)
+
+export const Alert = ({ alert, message, color }) => {
+  const onClose = () => document.getElementById('co').style.display='none'
+  return alert ? (
+    <Div id="co" style={{ backgroundColor: `${color}` }} class="alert-container">
+      <button onClick={onClose} class="dismiss-btn">
+        <i class="fa fa-close"></i>
+      </button>
+      <P>{message}</P>
+    </Div>
+  ) : null
+}
