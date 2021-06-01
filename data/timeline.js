66b36ae406be05adc990445dbccd985b8cf5633a
emmadal <emmanueldalougou@gmail.com>
2021-06-01 21:59:58 +0000

    timeline table and field in settinfs.jsx

---
+++ data/timeline.js
@@ -0,0 +1,18 @@
+export const timeline = [
+  {
+    id: 1,
+    challenge: 'Build markdown preview',
+    solution: '',
+    date: null
+  },
+  {
+    id: 2,
+    challenge: 'Build a Random Quote Machine',
+    solution: 'https://google.com',
+    date: new Date().toLocaleString('en-US', {
+      day: 'numeric',
+      month: 'short',
+      year: 'numeric'
+    })
+  }
+]
--- page/profile.jsx
+++ page/profile.jsx
@@ -1,8 +1,12 @@
+import { useState, useEffect } from 'preact/hooks'
 import { Div, divider, P } from '../component/elements.jsx'
 import { Img } from '../component/image.jsx'
 import { Layout } from '../component/layout.jsx'
+import { TimelineTable } from '../component/timeline-table.jsx'
+import { timeline } from '../data/timeline.js'
 import { user } from '../lib/auth.js'
 import { css } from '../lib/dom.js'
+
 css(`
 .user-info{
   display: flex;
@@ -13,35 +17,51 @@ css(`
   margin: 5px 0px;
   font-size: 1.2rem;
 }
-.user-timeline{
-}
 .user-img{
   border-radius: 50%;
+  margin: 10px 0px;
 }
 `)
 
-export const Profile = () => (
-  <Layout>
-    <Div class="user-info">
-      <Img
-        uri="https://randomuser.me/api/portraits/women/13.jpg"
-        size={150}
-        className="user-img"
-      />
-      <h1 style={{ fontSize: '1.5rem' }}>{user.name}</h1>
-      <span class="_info">
-        <i class="fa fa-envelope"></i> {user.email}
-      </span>
-      <span class="_info">
-        <i class="fa fa-map-marker"></i> {user.location}
-      </span>
-      <P class="_info">{user.biography}</P>
-      <span class="_info">
-        <i class="fa fa-calendar"></i> Joined {new Date().toGMTString()}
-      </span>
-    </Div>
-    {divider}
-    <h2 style={{ textAlign: 'center', fontSize: '1.5rem' }}>Timeline</h2>
-    <Div class="user-timeline"></Div>
-  </Layout>
-)
+export const Profile = () => {
+  const [data, setData] = useState([])
+  const getTimeline = (item) => item
+
+  useEffect(() => setData(timeline), [])
+
+  const columns = Object.entries({
+    challenge: {
+      size: '50%',
+      type: '',
+      bgcolor: 'var(--orange-dark)'
+    },
+    solution: { size: '25%', type: '', bgcolor: 'var(--pink-dark)', color: '' },
+    date: { size: '25%', type: '', bgcolor: 'var(--purple-lighter)' }
+  })
+
+  return (
+    <Layout>
+      <Div class="user-info">
+        <Img
+          uri="https://randomuser.me/api/portraits/women/13.jpg"
+          size={150}
+          className="user-img"
+        />
+        <h1 style={{ fontSize: '1.5rem' }}>{user.name}</h1>
+        <span class="_info">
+          <i class="fa fa-envelope"></i> {user.email}
+        </span>
+        <span class="_info">
+          <i class="fa fa-map-marker"></i> {user.location}
+        </span>
+        <P class="_info">{user.biography}</P>
+        <span class="_info">
+          <i class="fa fa-calendar"></i> Joined {new Date().toGMTString()}
+        </span>
+      </Div>
+      {divider}
+      <h2 style={{ textAlign: 'center', fontSize: '1.5rem' }}>Timeline</h2>
+      <TimelineTable data={data.map(getTimeline)} columns={columns} />
+    </Layout>
+  )
+}
