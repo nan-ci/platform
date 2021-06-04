7d09c93db1f6aaefc87faf291b9b6f8135db76eb
Ãramseycoder <ramseykoffi080@gmail.com>
2021-06-04 16:15:46 +0000

    add lesson page

--- data/courses.js
+++ data/courses.js
@@ -2,10 +2,8 @@ export const courses = [
   {
     id: 1,
     name: 'python',
-    title: 'Getting started with javascript',
     description: '',
-    content: [],
-    curriculum: [
+    modules: [
       { id: 1, title: 'Scientific Computing with Python', hours: 300 },
       { id: 2, title: 'Data Analysis with Python', hours: 300 },
       { id: 3, title: 'Machine Learning with Python', hours: 300 },
@@ -38,12 +36,96 @@ export const courses = [
   {
     id: 2,
     name: 'javascript',
-    title: 'Learn Javascript',
     description: '',
-    content: [],
-    curriculum: [
-      { id: 1, title: 'JavaScript Algorithms and Data Structures', hours: 300 },
-      { id: 2, title: 'APIs and Microservices', hours: 200 },
+    modules: [
+      { id: 1, title: 'JavaScript Algorithms and Data Structures', hours: 300,project: null },
+      { id: 2, title: 'APIs and Microservices', hours: 200,project: null },
+    ],
+    cours: [
+      {
+        id: 1,
+        idModule: 1,
+        title: 'introduction to variables',
+        description:
+          'this cours is an introduction to variables , here you will learn how to declare variables ',
+        videoLink: 'https://www.youtube.com/embed/1jT3EQVJOBA',
+        ressources:[
+          {
+          name:"livre pdf",
+          link:""
+        },
+        {
+          name:"recherche mdn",
+          link:"https://djmjdfkjdf"
+        }
+      ],
+        quizz: [],
+        livecoding: [],
+      },
+      {
+        id: 2,
+        idModule: 1,
+        title: 'data types of variables',
+        description: 'this cours talk about of data types in variables',
+        videoLink:  'https://www.youtube.com/embed/1jT3EQVJOBA',
+        ressources:[],
+        quizzs: [{}],
+        livecoding: null,
+      },
+      {
+        id: 2,
+        idModule: 1,
+        title: 'data types of variables',
+        description: 'this cours talk about of data types in variables',
+        videoLink: 'https://qkdfmjqdflj',
+        quizz: {},
+        livecoding: null,
+      },
+      {
+        id: 2,
+        idModule: 1,
+        title: 'data types of variables',
+        description: 'this cours talk about of data types in variables',
+        videoLink: 'https://qkdfmjqdflj',
+        quizz: {},
+        livecoding: null,
+      },
+      {
+        id: 2,
+        idModule: 1,
+        title: 'data types of variables',
+        description: 'this cours talk about of data types in variables',
+        videoLink: 'https://qkdfmjqdflj',
+        quizz: {},
+        livecoding: null,
+      },
+      {
+        id: 2,
+        idModule: 1,
+        title: 'data types of variables',
+        description: 'this cours talk about of data types in variables',
+        videoLink: null,
+        quizz: {},
+        livecoding: null,
+      },
+      {
+        id: 3,
+        idModule: 2,
+        title: 'Api',
+        description: 'this cours talk about of Api in variables',
+        videoLink: 'https://www.youtube.com/embed/1jT3EQVJOBA',
+        quizz: {},
+        livecoding: null,
+      },
+      {
+        id: 4,
+        idModule: 2,
+        title: 'Micro services',
+        description: 'this cours talk about micro services in variables',
+        videoLink: 'https://qkdfmjqdflj',
+        quizz: {},
+        livecoding: null,
+      },
     ],
     challenges: [
       {
--- page/home.jsx
+++ page/home.jsx
@@ -1,11 +1,64 @@
 import { logo } from '../data/ascii.js'
-import { P } from '../component/elements.jsx'
-import { Layout } from '../component/layout.jsx'
+import { Div, P } from '../component/elements.jsx'
+import { Layout } from '../component/Layout.jsx'
 import { user } from '../lib/auth.js'
+import { css } from '../lib/dom.js'
 
+css(`
+.container {
+  display:flex;
+  flex-direction:row;
+  align-items:center;
+  justify-content:space-evenly;
+}
+   .div {
+     border:1px solid white;
+     border-radius:25px;
+     padding:0.4rem;
+     width:300px;
+     height: 200px;
+   }
+   .buyFormation {
+     padding:0.8rem;
+     outline: none;
+     cursor:pointer;
+     background:var(--comment-darker);
+     margin:0 auto;
+     margin-top:20px;
+     border-radius:0.4rem;
+   }
+`)
+const getStudentStatus = () => {
+  localStorage.user = JSON.stringify({
+    ...JSON.parse(localStorage.user),
+    role: 'student',
+  })
+  location.href = '/'
+}
 export const Home = () => (
   <Layout>
-    <P fg="comment">{logo}</P>
-    <span>Akwaba {user.name}</span>
+    <br />
+    <br />
+    {user && (
+      <div style={{ textAlign: 'center', marginBottom: '40px' }}>
+        {user.role === 'visitor' ? (
+          <>
+            <h1>
+              you are enregistred like a visitor , please get the student status
+              {'\n'} if you want to have full access
+            </h1>
+            <button class="buyFormation" onClick={getStudentStatus}>
+              Get student status
+            </button>
+          </>
+        ) : (
+          <>
+            <P class="username-curriculum" style={{ fontSize: '1.2rem' }}>
+              Welcome back 👋, {user.name}
+            </P>
+          </>
+        )}
+      </div>
+    )}
   </Layout>
 )
