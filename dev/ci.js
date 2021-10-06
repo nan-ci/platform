ac3a512737015a3b10ee51901e895d41eb4a3d3f
Clement Denis <le.mikmac@gmail.com>
2021-10-06 13:12:06 +0200

    dev/ci: write logs to a file watched by papertrail

--- dev/ci.js
+++ dev/ci.js
@@ -1,5 +1,5 @@
 import { createServer } from 'https'
-import { readFileSync, writeFileSync, createReadStream } from 'fs'
+import { readFileSync, writeFileSync } from 'fs'
 import { spawn, spawnSync } from 'child_process'
 import { once } from 'events'
 
@@ -20,7 +20,7 @@ const read = async (stream) => {
 }
 
 const logToFile = (hash, content) =>
-  writeFileSync(`/tmp/nan-${hash}.log`, content, { flag: 'a' }) || content
+  writeFileSync(`/var/log/nan/ci_${hash}.log`, content, { flag: 'a' }) || content
 
 const allowedHeaders = [
   'x-nan-cookie',
@@ -33,9 +33,6 @@ const allowedHeaders = [
 const handleRequest = async (req, res, again) => {
   const hash = req.url.split('/', 2)[1]
   const url = req.url.slice(41)
-  if (/^\/log(\?|\/)?/.test(url)) {
-    return createReadStream(`/tmp/nan-${hash}.log`).pipe(res)
-  }
   const { method, headers } = req
   const version = headers.referer?.match(
     /https:\/\/([a-z0-9]{8})\.platform-nan-dev-8sl\.pages\.dev/,
