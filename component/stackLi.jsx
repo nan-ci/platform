e1198f415ba165eda2f7fe6af357b4081390e215
ramseycodercoder <ramseykoffi080@gmail.com>
2021-10-18 22:14:54 +0000

    fix: review correction

--- component/stackLi.jsx
+++ component/stackLi.jsx
@@ -1,5 +1,5 @@
 import { css } from '../lib/dom.js'
-import { Span } from './elements'
+import { Div, Span } from '../component/elements.jsx'
 
 css(`
   .module .stackli {
@@ -8,23 +8,45 @@ css(`
       display: flex;
       flex-direction: row;
       align-items: center;
-      justify-content: flex-start;
-      border: 2px solid white;
+      justify-content: space-between;
+      border: 2px solid grey;
       margin: 10px auto;
-  }
-  .module .stackli span.square {
-      width:  20px;
-      height: 20px;
-      background: white;
-      border-radius: 0.3rem;
-      margin-right: 15px;
+      cursor: not-allowed;
   }
 
-  .module .stackli span.circle {
-    width:20px;
-    height:20px;
-    border-radius: 5rem;
+  .module .stackli span {
+    color: grey;
+    font-weight: bolder;
+  }
+
+  .module .stackli.pass {
+    border-color: white;
     background: white;
+    cursor: pointer;
+  }
+
+  .module .stackli.next {
+    border-color: white;
+    cursor: pointer;
+  }
+
+  .module .stackli.pass span {
+    color: var(--comment-darker);
+  }
+
+  .module .stackli.next span {
+    color: white;
+  }
+
+  .module .stackli div  {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start
+  }
+
+  .module .stackli span.emoji {
+   font-size: 1.2rem;
     margin-right: 15px;
   }
 
@@ -32,13 +54,32 @@ css(`
    font-size: 1.2rem;
    font-weight: bolder;
  }
+
+ .module .stackli span.attempts {
+   margin-right: 10px;
+ }
 `)
 
-export const StackLi = ({ data: { type, name } }) => {
+export const StackLi = ({ data: { type, name }, isPassed, isNext, infos }) => {
+  const iconType = {
+    quiz: '📚',
+    exercise: '🖊',
+  }
+
   return (
-    <li class="stackli">
-      <Span class={`${type === 'exercise' ? 'square' : 'circle'}`} />
-      <Span class="name">{name}</Span>
+    <li class={`stackli ${isPassed ? 'pass' : isNext ? 'next' : ''}`}>
+      <Div class="first_block">
+        <Span class="emoji">{iconType[type]}</Span>
+        <Span class="name">{name}</Span>
+      </Div>
+      {infos && (
+        <Div class="second_block">
+          <Span class="attempts">{infos.attempts}-attempts</Span>
+          {infos.seconds && (
+            <Span class="seconds">{infos.seconds}-seconds</Span>
+          )}
+        </Div>
+      )}
     </li>
   )
 }
--- page/module.jsx
+++ page/module.jsx
@@ -1,11 +1,20 @@
 import { css } from '../lib/dom.js'
 import { Div, P } from '../component/elements.jsx'
 import { StackLi } from '../component/stackLi.jsx'
+import fakeModule from '../data/fakeModule.json'
+import userInfo from '../data/fakeUserData.json'
 
 css(`
    .module h1 {
      font-size: 2rem;
      font-weight: bolder;
+    }
+
+    .module>hr {
+      width: 100%;
+      height: 2px;
+      margin: 7px auto;
+      background: grey;
     }
 
    .module p.description {
@@ -54,36 +63,58 @@ css(`
   }
 `)
 
-const fakeModule = {
-  name: 'JS Basics',
-  description:
-    'this  module talk about the basics  programmation in javascript',
-  notions: ['variables', 'data types', 'operators', 'condition', 'iteraction'],
-  exercises: [
-    '01-Hello There',
-    '02-Anything to Declare',
-    '03-Good Search',
-    '05-Redeclaration of Love',
-    '06-Duplicate',
-  ],
-  quizzes: ['04-World of DOM', '07-Cool'],
-  configs: {
-    display: [
-      { name: '01-hello There', type: 'exercise' },
-      { name: '02-Anything to Declare', type: 'exercise' },
-      { name: '03-Good Search', type: 'exercise' },
-      { name: '04-World of DOM', type: 'quiz' },
-      { name: '05-Redeclaration Love', type: 'exercise' },
-      { name: '06-Duplicate', type: 'exercise' },
-      { name: '07-cool', type: 'quiz' },
-    ],
-  },
-}
-
 export const Module = () => {
+  const getExoNumber = (name) => parseInt(name.split('-')[0])
+
+  const getInfos = (name) => {
+    const userProgress = Object.entries(userInfo.results)
+    return userProgress.find(
+      (r) => getExoNumber(r[0]) === getExoNumber(name),
+    )[1]
+  }
+
+  const isPassed = (name) => {
+    const [module, step] = userInfo.level
+      .split('-')
+      .map((r) => parseInt(r.slice(1)))
+    const userProgress = Object.entries(userInfo.results)
+    const moduleNumber = getExoNumber(fakeModule.name)
+    const exoNumber = getExoNumber(name)
+    if (
+      module > moduleNumber ||
+      (module === moduleNumber &&
+        step >= exoNumber &&
+        userProgress.find((r) => getExoNumber(r[0]) === exoNumber)[1].pass)
+    ) {
+      return true
+    } else return false
+  }
+
+  const isNext = (name) => {
+    const [module, step] = userInfo.level
+      .split('-')
+      .map((r) => parseInt(r.slice(1)))
+    const userProgress = Object.entries(userInfo.results)
+    const exoNumber = getExoNumber(name)
+    if (module === getExoNumber(fakeModule.name)) {
+      if (
+        step === exoNumber &&
+        !userProgress.find((r) => getExoNumber(r[0]) === exoNumber)[1]?.pass
+      ) {
+        return { info: true }
+      } else if (
+        exoNumber === step + 1 &&
+        userProgress.find((r) => getExoNumber(r[0]) === step)[1]?.pass
+      ) {
+        return true
+      }
+    } else return false
+  }
+
   return (
     <Div class="module">
       <h1> {fakeModule.name}</h1>
+      <hr />
       <P class="description">{fakeModule.description}</P>
       <P class="title"> In this module you will learn : </P>
       <ul class="notions">
@@ -94,8 +125,18 @@ export const Module = () => {
       <Div class="stack-content">
         <h3> Exercises </h3>
         <ul class="stack">
-          {fakeModule.configs.display.map((s) => (
-            <StackLi key={s.name} data={s} />
+          {fakeModule.children.map(({ key, ...rest }) => (
+            <StackLi
+              key={key}
+              data={rest}
+              isPassed={isPassed(rest.name)}
+              isNext={isNext(rest.name)}
+              infos={
+                isPassed(rest.name) || isNext(rest.name)?.info
+                  ? getInfos(rest.name)
+                  : null
+              }
+            />
           ))}
         </ul>
       </Div>
