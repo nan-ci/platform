cae08f8dc1cd00da9e6335bddf1ea4ebcc3d12d6
ramseycoder <ramseykoffi080@gmail.com>
2021-10-26 09:56:21 +0000

    add quiz page

--- app.jsx
+++ app.jsx
@@ -6,12 +6,17 @@ import { P, divider } from './component/elements.jsx'
 import { Router } from './lib/router.js'
 import { Profile } from './page/profile.jsx'
 import { Home } from './page/home.jsx'
+<<<<<<< HEAD
 
+=======
+import { Quiz } from './page/quiz.jsx'
+>>>>>>> add quiz page
 const App = () => (
   <>
     <Header />
     <Router>
       <Profile path="/profile" />
+      <Quiz path="/quiz" />
       <Home path="*" />
     </Router>
     <footer>
---
+++ page/quiz.jsx
@@ -0,0 +1,16 @@
+import { Div } from '../component/elements.jsx'
+import { MTitle } from '../component/markdown.jsx'
+import { css } from '../lib/dom.js'
+
+css(`
+
+`)
+
+export const Quiz = () => {
+  return (
+    <Div class="quiz">
+      <MTitle.h2>07 - Cool</MTitle.h2>
+      {'\n'}
+    </Div>
+  )
+}
